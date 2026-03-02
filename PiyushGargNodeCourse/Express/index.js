3895b1039ba96dffba20a107f5e0e6109b972d1d
venkataManikanta07 <dv.manikanta07@gmail.com>
2026-03-03 01:37:04 +0530

    Versioning notes has been added

--- PiyushGargNodeCourse/Express/index.js
+++ PiyushGargNodeCourse/Express/index.js
@@ -23,3 +23,13 @@ app.get("/about", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 })
+
+
+// Versioning in Packages 
+/**
+ * 4.15.0 - Major version 
+ * 4 is the major version, 15 is the minor version and 0 is the patch version.
+ * caret ^ - allows updates that do not change the leftmost non-zero digit in the version number. For example, ^4.15.0 would allow updates to any version from 4.15.0 to less than 5.0.0.
+ * tilde ~ - allows updates that do not change the leftmost non-zero digit in the minor version number. For example, ~4.15.0 would allow updates to any version from 4.15.0 to less than 4.16.0.
+ * exact version - allows only that specific version to be installed. For example, 4.15.0 would only allow version 4.15.0 to be installed.
+ */
