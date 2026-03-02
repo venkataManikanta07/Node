5ee43d35b4083c0fe7c1859237908483c1f34be4
venkataManikanta07 <dv.manikanta07@gmail.com>
2026-03-03 01:57:11 +0530

    Rest API Theory - SSR - CSR

--- PiyushGargNodeCourse/Express/index.js
+++ PiyushGargNodeCourse/Express/index.js
@@ -33,3 +33,15 @@ app.listen(3000, () => {
  * tilde ~ - allows updates that do not change the leftmost non-zero digit in the minor version number. For example, ~4.15.0 would allow updates to any version from 4.15.0 to less than 4.16.0.
  * exact version - allows only that specific version to be installed. For example, 4.15.0 would only allow version 4.15.0 to be installed.
  */
+
+
+/**
+ * REST API - Representational State Transfer Application Programming Interface
+ *  . Client make a request to the server, server processes the request and sends back a response to the client.
+ * 
+ * SSR - Client make GET request to the server, server renders the HTML and sends it back to the client.
+ *  . Fast to load the page because the server is doing all the work of rendering the HTML.
+ * 
+ * CSR - Client make a GET request to the server, server sends back the HTML, CSS and JavaScript files to the client, client renders the HTML and executes the JavaScript to make API calls to the server to get the data and render it on the page.
+ *  . Slower to load the page because the client is doing all the work of rendering the HTML and executing the JavaScript.
+ */
