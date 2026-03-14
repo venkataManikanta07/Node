dde5323621e5ea79498e317a6285f50882496a53
Manikanta Dv. <111730826+venkataManikanta07@users.noreply.github.com>
2026-03-14 14:00:52 +0530

    middleware definition

--- PiyushGargNodeCourse/Project1Ep13/index.js
+++ PiyushGargNodeCourse/Project1Ep13/index.js
@@ -15,3 +15,9 @@ app.get("/users", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+
+
+// const app = express(); - creates an instance of the express application. This instance is used to define routes and middleware for the application.
+
+// app.use(express.urlencoded({ extended: true })); - This line of code is used to parse incoming request bodies in a middleware before your handlers, available under the req.body property. The extended option allows for rich objects and arrays to be encoded into the URL-encoded format, which can be useful for complex data structures.
